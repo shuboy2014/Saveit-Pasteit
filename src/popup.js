30d1bea29ddc2197b12f242fc2490f1166a9d0b7
Shubham Aggarwal <shubham.aggarwal@comprotechnologies.com>
2020-03-09 12:40:42 +0530

    rm unwanted code

--- src/popup.js
+++ src/popup.js
@@ -28,45 +28,49 @@ const id_to_image = {
     "22": "align-left"
 };
 
-const image_to_id = {
-    "facebook-square": "1",
-    "linkedin-square": "2",
-    "dropbox": "3",
-    "envelope": "4",
-    "github-alt": "5",
-    "yahoo": "6",
-    "wordpress": "7",
-    "youtube-square": "8",
-    "reddit": "9",
-    "mobile": "10",
-    "stack-overflow": "11",
-    "hand-o-right": "12",
-    "play-circle": "13",
-    "skype": "14",
-    "google-plus-official": "15",
-    "twitter": "16",
-    "instagram": "17",
-    "whatsapp": "18",
-    "slack": "19",
-    "pinterest": "20",
-    "th-large": "21",
-    "align-left": "22"
-};
-
 $('document').ready(function () {
 
-    function get_id() {
+    (() => {
+        $('.saved-links-container').slimScroll({
+            height: '400px',
+            alwaysVisible: true,
+            color: '#34495E'
+        });
+
+
+        const linksList = JSON.parse(localStorage.getItem("saved_data"));
+        linksList.reverse();
+        for (let link of linksList) {
+            const elementHtml = `
+                <div class="container-fluid" id="${link.id + "-div"}">
+                    <p><strong id="${link.id}-name">${link.name}</strong></p>
+                    <div class="saved-input-box">
+                        <span><i class="fa fa-${link.icon} fa-2x"></i></span>
+                        <input type="text" class="form-control" id="${link.id}-url" value="${link.linkUrl}" autocomplete="off" spellcheck="false">
+                        <button type="button"><span class="copytext" id="${link.id}-copy"><i class="fa fa-clipboard"></i></span></button>
+                        <button type="button"><span  class="remove" id="${link.id}-btn"><i class="fa fa-times"></i></span></button>
+                     </div>
+                     <br>
+                </div>
+            `;
+            $('.saved-links-container').append(elementHtml);
+        }
+    })();
+
+    function getNewId(linksList) {
+        let maxId = 0;
+        for (const link of linksList) {
+            if (link.id > maxId) {
+                maxId = link.id;
+            }
+        }
+
+        return maxId + 1;
+    }
+
+    function getLinkIcon() {
         const classAttr = $('#new-link-i').attr('class');
-        let imageName = "";
-        for (let index = 6; index < classAttr.length - 19; index++) {
-            imageName += classAttr[index];
-        }
-
-        if (!imageName) {
-            imageName = 'hand-o-right';
-        }
-
-        return image_to_id[imageName];
+        return classAttr.slice(6, classAttr.length - 19);
     }
 
     function copyTextToClipboard(text) {
@@ -78,58 +82,36 @@ $('document').ready(function () {
         copyFrom.remove();
     }
 
-    function slugify(text) {
-        return text.toString().toLowerCase()
-            .replace(/\s+/g, '-')           // Replace spaces with -
-            .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-            .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-            .replace(/^-+/, '')             // Trim - from start of text
-            .replace(/-+$/, '');            // Trim - from end of text
-    }
-
-    $(function () {
-        $('.saved-links-container').slimScroll({
-            height: '400px',
-            alwaysVisible: true,
-            color: '#34495E'
-        });
-    });
-
-    function remove_it() {
-        let linkId = this.id;
-        let linkName = linkId.substr(0, linkId.length - 4);
+    function removeLink() {
+        const elementId = this.id;
+        const linkId = elementId.substr(0, elementId.length - 4); // block id = '${linkId}-btn'
         const linksList = JSON.parse(localStorage["saved_data"]);
         for (let index in linksList) {
-            if (linksList[index].name === linkName) {
-                linksList.splice(index, 1);
+            if (linksList[index].id.toString() === linkId) {
+                linksList.splice(parseInt(index, 10), 1);
             }
         }
         localStorage.setItem("saved_data", JSON.stringify(linksList));
-        const div_element = "#" + slugify(linkName + "-div");
-        $(div_element).remove();
+        $(`#${linkId}-div`).remove();
         chrome.runtime.sendMessage({"task": "removeIt"});
     }
 
-    function copy_it() {
-        let copyText = document.getElementById(this.id).value;
-        document.getElementById('message').innerHTML = this.id + ' Copied';
+    function copyLink() {
+        const elementId = this.id;
+        const linkId = elementId.substr(0, elementId.length - 5); // block id = '${linkId}-copy'
+        const linkUrl = document.getElementById(`${linkId}-url`).value;
+        const linkName = document.getElementById(`${linkId}-name`).innerText;
+        document.getElementById('message').innerHTML = `${linkName} copied`;
+        copyTextToClipboard(linkUrl);
         $('#myModal').modal('show');
-        copyTextToClipboard(copyText);
     }
 
-    function save_information() {
+    function saveNewLink() {
         const linkName = $('input[name="new-name"]').val();
         const linkUrl = $('input[name="new-link"]').val();
         if (linkName && linkUrl) {
             const linksList = JSON.parse(localStorage.getItem("saved_data"));
-            const newLink = {"id": get_id(), "name": linkName, 'linkUrl': linkUrl};
-            // TO check newly added information is already present or not
-            for (let link of linksList) {
-                if (linkName === link.name) {
-                    alert('Oops , ' + linkName + ' already exist...!!!');
-                    return;
-                }
-            }
+            const newLink = {"id": getNewId(linksList), icon: getLinkIcon(), name: linkName, linkUrl: linkUrl};
             linksList.push(newLink);
             localStorage.clear();
             localStorage.setItem('saved_data', JSON.stringify(linksList));
@@ -139,47 +121,37 @@ $('document').ready(function () {
         }
     }
 
-    const linksList = JSON.parse(localStorage.getItem("saved_data"));
-    linksList.reverse();
-    for (let link of linksList) {
-        let div_id = link.name + "-div";
-        let remove_btn = link.name + "-btn";
-        let copy_element_id = link.name;
-        let element = '<div  class="container-fluid" id="' + slugify(div_id) + '"><p><strong>' + link.name + '</strong></p><div class="saved-input-box"><span><i class="fa fa-' + id_to_image[link.id] + ' fa-2x"></i></span><input type="text" class="form-control" id="' + link.name + '" value="' + link.linkUrl + '" autocomplete="off" spellcheck="false"><button><span class="copytext" id="' + copy_element_id + '"><i class="fa fa-clipboard "></i></span></button><button><span  class="remove" id="' + remove_btn + '"><i class="fa fa-times"></i></span></button></div><br></div>';
-        $('.saved-links-container').append(element);
-    }
-
     $('.change-image').on("click", function () {
         $('#new-name-i').attr("class", "fa fa-" + id_to_image[this.id] + " fa-2x change-image");
         $('#new-link-i').attr("class", "fa fa-" + id_to_image[this.id] + " fa-2x change-image");
     });
 
     $('#save-add-another').on("click", function () {
-        save_information();
+        saveNewLink();
         document.getElementById('new-name-input').value = "";
         document.getElementById('new-link-input').value = "";
         $("#new-info-form-modal").modal('show');
     });
 
-    $('#add-new-information-btn').click(function () {
+    $('#add-new-information-btn').on('click', function () {
         document.getElementById('new-name-input').value = "";
         document.getElementById('new-link-input').value = "";
         $('#new-info-form-modal').modal('show');
     });
 
-    $('#cancel').click(function () {
+    $('#cancel').on("click", function () {
         $('#new-info-form-modal').modal('hide');
     });
 
-    $('.remove').on("click", remove_it);
+    $('.remove').on("click", removeLink);
 
-    $('.copytext').on('click', copy_it);
+    $('.copytext').on('click', copyLink);
 
-    $('#save').click(function () {
-        save_information();
+    $('#save').on("click", function () {
+        saveNewLink();
     });
 
-    $('#saveit-btn').click(function () {
+    $('#saveit-btn').on("click", function () {
         const save_btn = $('#saveit-btn');
         save_btn.prop('disabled', true);
         const linksList = JSON.parse(localStorage["saved_data"]);
@@ -208,4 +180,4 @@ $('document').ready(function () {
         document.getElementById('message').innerHTML = ' Saved Successfully ';
         $('#myModal').modal('show');
     });
-}); 
+});
